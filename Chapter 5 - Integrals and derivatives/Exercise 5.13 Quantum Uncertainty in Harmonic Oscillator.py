81b9c25d6529e050085a12eef42f2b88fdbd3195
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2026-07-28 21:49:52 +0100

    Complete 5.13

--- Chapter 5 - Integrals and derivatives/Exercise 5.13 Quantum Uncertainty in Harmonic Oscillator.py
+++ Chapter 5 - Integrals and derivatives/Exercise 5.13 Quantum Uncertainty in Harmonic Oscillator.py
@@ -9,13 +9,23 @@ def h(n,x):
         return 1
     if n == 1:
         return 2*x
-    return 2*x*h(n-1,x) - 2*(n-1)*h(n-2,x)
+
+    h0 =1 
+    h1 = 2*x
+    for k in range (2,n+1):
+        h2 = 2*x*h1 - 2*(k - 1)*h0
+        h0 = h1
+        h1 = h2
+    return h2
 
 def psi(n,x):
     a = (1)/(np.sqrt((2**n)*spec.factorial(n)*np.sqrt(np.pi)))
     b = np.exp((-x**2)/2)
     c= h(n,x)
     return a*b*c
+
+def f(n,x):
+    return x**2 * np.abs(psi(n,x))**2
 
 x_vals = np.linspace(-4,4,100)
 
@@ -27,14 +37,19 @@ psi2 = [psi(2, x) for x in x_vals]
 
 psi3 = [psi(3, x) for x in x_vals]
 
-#plt.plot(x_vals,psi0 , label = "$\psi_0$")
-#plt.plot(x_vals,psi1 , label = "$\psi_1$")
-#plt.plot(x_vals,psi2 , label = "$\psi_2$")
-#plt.plot(x_vals,psi3 , label = "$\psi_3$")
-#plt.legend()
-#plt.show()
+plt.plot(x_vals,psi0 , label = "$\psi_0$")
+plt.plot(x_vals,psi1 , label = "$\psi_1$")
+plt.plot(x_vals,psi2 , label = "$\psi_2$")
+plt.plot(x_vals,psi3 , label = "$\psi_3$")
+plt.legend()
+plt.show()
 
-x_vals = np.linspace(-10,10,100)
+x_vals = np.linspace(-10,10,1000)
 psilst = [psi(30, x) for x in x_vals]
 plt.plot(x_vals,psilst)
 plt.show()
+
+integrand = lambda z: (1+z**2)/((1-z**2)**2) * f(5,((z)/(1-z**2)))
+s,_ = fixed_quad(integrand, -1, 1, n=100)
+
+print(np.sqrt(s))
