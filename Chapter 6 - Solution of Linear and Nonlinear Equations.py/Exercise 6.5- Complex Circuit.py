19ade9a49aed4a912f6f2375944d092e937e5e24
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2026-07-31 11:18:49 +0100

    Completed 6.5

--- Chapter 6 - Solution of Linear and Nonlinear Equations.py/Exercise 6.5- Complex Circuit.py
+++ Chapter 6 - Solution of Linear and Nonlinear Equations.py/Exercise 6.5- Complex Circuit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from cmath import polar
 R = [1000,2000,1000,2000,1000,2000]
 C1 = 1e-6
 C2= 0.5e-6
@@ -14,10 +14,8 @@ A = np.array([
 ])
 
 v = np.array([X_plus/R[0], X_plus/R[1], X_plus/R[2]])
-
 x = np.linalg.solve(A,v)
 
-print(x)
 t= np.linspace(0,0.05,1000)
 
 V_plus = X_plus*np.exp(1j*w*t)
@@ -25,12 +23,25 @@ V1= x[0]*np.exp(1j*w*t)
 V2= x[1]*np.exp(1j*w*t)
 V3= x[2]*np.exp(1j*w*t)
 
-plt.plot(t,V_plus,label="V+")
-plt.plot(t,V1,label="V1")
-plt.plot(t,V2, label="V2")
-plt.plot(t,V3,label="V3")
+AmpV1, ThetaV1 = polar(x[0])
+AmpV2, ThetaV2 = polar(x[1])
+AmpV3, ThetaV3 = polar(x[2])
+
+print(f"V1: Amplitude = {AmpV1:.4f} V, Phase = {np.degrees(ThetaV1):.2f}°")
+print(f"V2: Amplitude = {AmpV2:.4f} V, Phase = {np.degrees(ThetaV2):.2f}°")
+print(f"V3: Amplitude = {AmpV3:.4f} V, Phase = {np.degrees(ThetaV3):.2f}°")
+
+plt.plot(t, V_plus.real, label="V+")
+plt.plot(t, V1.real, label="V1")
+plt.plot(t, V2.real, label="V2")
+plt.plot(t, V3.real, label="V3")
+
 plt.legend()
 plt.xlabel("Time (s)")
 plt.ylabel("Voltage (V)")
 plt.grid()
 plt.show()
+
+#V1: Amplitude = 1.6684 V, Phase = -66.07°
+#V2: Amplitude = 1.6753 V, Phase = -15.94°
+#V3: Amplitude = 2.1054 V, Phase = -4.43°
